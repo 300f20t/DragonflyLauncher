ff13d27268e9050532d1892edd40cafe8d5beda6
300f20t <93136063+300f20t@users.noreply.github.com>
2024-09-27 16:28:13 +0300

    create a home page and a settings page

--- DragonflyLauncher/MainWindow.xaml.cs
+++ DragonflyLauncher/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Windows;
 using System.Windows.Navigation;
 using System.Windows.Interop;
+using DragonflyLauncher.Pages;
 
 namespace DragonflyLauncher
 {
@@ -13,84 +14,10 @@ namespace DragonflyLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-        string mineDir = @"%AppData%\.minecraft";
-        string login = "Player";
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private async void PlayClick(object sender, RoutedEventArgs e)
-        {
-            if (nickTextBox.Text != "") login = nickTextBox.Text;
-
-            System.Net.ServicePointManager.DefaultConnectionLimit = 256;
-
-            // initialize the launcher
-            var path = new MinecraftPath();
-            var launcher = new MinecraftLauncher(path);
-
-            // add event handlers
-            launcher.FileProgressChanged += (sender, args) =>
-            {
-                Console.WriteLine($"Name: {args.Name}");
-                Console.WriteLine($"Type: {args.EventType}");
-                Console.WriteLine($"Total: {args.TotalTasks}");
-                Console.WriteLine($"Progressed: {args.ProgressedTasks}");
-            };
-            launcher.ByteProgressChanged += (sender, args) =>
-            {
-                Console.WriteLine($"{args.ProgressedBytes} bytes / {args.TotalBytes} bytes");
-            };
-
-            // get all versions
-            var versions = await launcher.GetAllVersionsAsync();
-            foreach (var v in versions)
-            {
-                Console.WriteLine(v.Name);
-            }
-
-            // install and launch the game
-            await launcher.InstallAsync("1.20.6");
-            var process = await launcher.BuildProcessAsync("1.20.6", new MLaunchOption
-            {
-                Session = MSession.CreateOfflineSession(login),
-                MaximumRamMb = 4096
-            });
-            process.Start();
-        }
-
-        private void ModsButtonClick(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("WIP");
-        }
-
-        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
-        {
-            nickTextBox.Visibility = Visibility.Hidden;
-            accountsTextBlock.Text = "Login with";
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo("https://github.com/") { UseShellExecute = true });
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            string absoluteMineDir = Environment.ExpandEnvironmentVariables(mineDir);
-            Process.Start("explorer.exe", @$"{absoluteMineDir}");
-        }
-
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            nickTextBox.Visibility = Visibility.Visible;
-            accountsTextBlock.Text = "Login";
-        }
-
-        private void OpenSettings(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("WIP");
+            MainFrame.Content = new HomePage();
         }
     }
 }
---
+++ DragonflyLauncher/Pages/HomePage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using CmlLib.Core.Auth;
+using CmlLib.Core.ProcessBuilder;
+using CmlLib.Core;
+using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragonflyLauncher.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        string mineDir = @"%AppData%\.minecraft";
+        string login = "Player";
+
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PlayClick(object sender, RoutedEventArgs e)
+        {
+            if (nickTextBox.Text != "") login = nickTextBox.Text;
+
+            System.Net.ServicePointManager.DefaultConnectionLimit = 256;
+
+            // initialize the launcher
+            var path = new MinecraftPath();
+            var launcher = new MinecraftLauncher(path);
+
+            // add event handlers
+            launcher.FileProgressChanged += (sender, args) =>
+            {
+                Console.WriteLine($"Name: {args.Name}");
+                Console.WriteLine($"Type: {args.EventType}");
+                Console.WriteLine($"Total: {args.TotalTasks}");
+                Console.WriteLine($"Progressed: {args.ProgressedTasks}");
+            };
+            launcher.ByteProgressChanged += (sender, args) =>
+            {
+                Console.WriteLine($"{args.ProgressedBytes} bytes / {args.TotalBytes} bytes");
+            };
+
+            // get all versions
+            var versions = await launcher.GetAllVersionsAsync();
+            foreach (var v in versions)
+            {
+                Console.WriteLine(v.Name);
+            }
+
+            // install and launch the game
+            await launcher.InstallAsync("1.20.6");
+            var process = await launcher.BuildProcessAsync("1.20.6", new MLaunchOption
+            {
+                Session = MSession.CreateOfflineSession(login),
+                MaximumRamMb = 4096
+            });
+            process.Start();
+        }
+
+        private void ModsButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WIP");
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            nickTextBox.Visibility = Visibility.Hidden;
+            accountsTextBlock.Text = "Login with";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/") { UseShellExecute = true });
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string absoluteMineDir = Environment.ExpandEnvironmentVariables(mineDir);
+            Process.Start("explorer.exe", @$"{absoluteMineDir}");
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            nickTextBox.Visibility = Visibility.Visible;
+            accountsTextBlock.Text = "Login";
+        }
+
+        private void OpenSettings(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WIP");
+        }
+    }
+}
